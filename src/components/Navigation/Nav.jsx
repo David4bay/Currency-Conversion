8ea933248150d3062b5851c45c5c7779144a22b7
David4bay <david4bay@gmail.com>
2023-08-07 15:48:50 +0100

    nav styled components imported from utils folder

--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -1,44 +1,14 @@
-import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
-const HomeLink = styled.nav`
-    display: flex;
-    flex-direction: row-reverse;
-    color: white;
-    margin-bottom: 30px;
-    padding-bottom: 12px;
-    box-shadow: 0px 0px 8px 1px #fff;
-`
+import {
+    HomeLink,
+    NavContainer,
+    IconText,
+    Icon
+} from '../../util/util'
 
-const NavContainer = styled.ul`
-    padding-top: 1rem;
-    list-style: none;
-    padding-right: 5rem;
-    font-size: 22px;
-    transition: all 0.3s ease;
-    `
-    
-const IconText = styled.span`
-    padding-right: 12px;
-`
 
-const Icon = styled.li`
-    padding: 5px;
-
-    &:hover {
-        cursor: pointer;
-        background: hsla(100, 0%, 100%, 0.15);
-        backdrop-filter: blur(5px);
-        width: fit-content;
-        padding: 5px;
-    }
-
-    &:active {
-        color: #4FBBBF;
-        transform: translateY(5px);
-    }
-`
 
 function Nav() {
     return (
--- src/util/util.js
+++ src/util/util.js
@@ -85,7 +85,6 @@ box-shadow: -1px 0px 1px 0px #fff;
 background-color: hsla(0, 0%, 0%, 0.45);
 backdrop-filter: blur(5px);
 `
-
 export const Heading = styled.header`
     font-style: normal;
     font-weight: bolder;
@@ -135,4 +134,42 @@ export const CurrencyFont = {
 
 export const FontContainer = styled.span`
     display: block;
+`
+
+export const HomeLink = styled.nav`
+    display: flex;
+    flex-direction: row-reverse;
+    color: white;
+    margin-bottom: 30px;
+    padding-bottom: 12px;
+    box-shadow: 0px 0px 8px 1px #fff;
+`
+
+export const NavContainer = styled.ul`
+    padding-top: 1rem;
+    list-style: none;
+    padding-right: 5rem;
+    font-size: 22px;
+    transition: all 0.3s ease;
+    `
+    
+export const IconText = styled.span`
+    padding-right: 12px;
+`
+
+export const Icon = styled.li`
+    padding: 5px;
+
+    &:hover {
+        cursor: pointer;
+        background: hsla(100, 0%, 100%, 0.15);
+        backdrop-filter: blur(5px);
+        width: fit-content;
+        padding: 5px;
+    }
+
+    &:active {
+        color: #4FBBBF;
+        transform: translateY(5px);
+    }
 `
