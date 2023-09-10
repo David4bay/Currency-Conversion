eb0a83edc62d69e6e50b68021dd129cc3df21503
David4bay <david4bay@gmail.com>
2023-09-10 07:49:51 +0100

    fixed merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect, useCallback } from "react"
 import React, { useState, useEffect, useCallback } from "react"
 import LoadingState from "./util/LoadingState/LoadingState";
 const Body = React.lazy(() => import("./components/Body/Body"))
@@ -6,57 +5,6 @@ const Header = React.lazy(() => import("./components/Header/Header"))
 const Nav = React.lazy(() => import("./components/Navigation/Nav"))
 
 function App() {
-
-  const [convertedNumber, setConvertedNumber] = useState(null);
-  const [currencyRate, setCurrencyRate] = useState(null)
-  const [currency, setCurrency] = useState({
-    defaultType: 'USD',
-    defaultTypeAmount: 0,
-    convertedType: 'EUR',
-    convertedTypeAmount: 0
-  })
-  
-  useEffect(() => {
-    if (!currencyRate) {
-      fetch('../data/currencyData.json').then((response) => {
-        return response.json()
-      }).then((data) => {
-        for (const rate of data) {
-          if (rate["exchange_rate"]) {
-          setCurrencyRate(Math.floor(rate["exchange_rate"]))
-        }
-        }
-      })
-    }
-  }, [currencyRate])
-
-  useEffect(() => {
-      setConvertedNumber(Number(currency.defaultTypeAmount * currencyRate))
-      console.log(convertedNumber)
-  }, [convertedNumber, currency.defaultAmount, currency.defaultTypeAmount, currencyRate])
-  
-  const changeCurrencyHandler = (e) => {
-    const name = e.target.name;
-    const value = e.target.value.slice(0, 7);
-    const type = e.target.name === "defaultTypeAmount" ? currency.defaultType : currency.convertedType
-    setCurrency({ ...currency, [name]: Number(value.toLocaleString("en-US", { style: "currency", currency: type})) })
-  }
-  
-  const swapCurrencies = useCallback(() => {
-    const defaultTypes = currency.defaultType;
-    const defaultAmounts = currency.defaultTypeAmount;
-    const convertedTypes = currency.convertedType;
-    const convertedTypeAmounts = currency.convertedTypeAmount;
-  
-    setCurrency({
-        ...currency, 
-        defaultType: convertedTypes, 
-        defaultTypeAmount: convertedTypeAmounts, 
-        convertedType: defaultTypes, 
-        convertedTypeAmount: defaultAmounts
-    })
-  }, [currency])
-
 
   const [defaultNumber, setDefaultNumber] = useState(null);
   const [convertedNumber, setConvertedNumber] = useState(null);
@@ -124,13 +72,8 @@ function App() {
     currency={currency}
     convertedNumber={convertedNumber}
     swapCurrencies={swapCurrencies} 
-    changeCurrencyHandler={changeCurrencyHandler}  
-    
+    changeCurrencyHandler={changeCurrencyHandler}
     defaultNumber={defaultNumber}
-    currency={currency}
-    convertedNumber={convertedNumber}
-    swapCurrencies={swapCurrencies} 
-    changeCurrencyHandler={changeCurrencyHandler}  
     />
     </main>
     </React.Suspense>
